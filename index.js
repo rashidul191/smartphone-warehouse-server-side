4e0ecac57c945ca47c100271f7465fa3b3e997a1
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-06-28 00:18:56 +0600

    delete item date from database

--- index.js
+++ index.js
@@ -47,6 +47,14 @@ async function run() {
       const result = await productCollection.insertOne(newProduct);
       res.send(result);
     });
+
+    // DELETE single data form database
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
